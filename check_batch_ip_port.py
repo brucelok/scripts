a23b659a346218a65de480a91caa5ed888c07f1e
Bruce <lok.bruce@gmail.com>
2019-11-25 06:18:16 +0000

    add missing argument warning message

--- check_batch_ip_port.py
+++ check_batch_ip_port.py
@@ -1,14 +1,21 @@
-import socket
+import sys, socket
 '''
 a simple tool that read a list of IP addresses/ports from a plaintext file
 and verify the remote IP/Port connection.
 example file format (comma is required as a delimiter btw IP and Port):
 10.0.0.1,8443
 10.0.0.2,22
+syntax:
+$ python check_batch_ip_port.py <text filename>
 author: lok.bruce@gmail.com
 '''
+if len(sys.argv) < 2:
+    sys.stderr.write("Usage: $ python %s <text_filename>" % sys.argv[0])
+    sys.exit(1)
 
-with open('ls_ipport', 'r') as f:
+textfile = sys.argv[1]  #take filename in same directory
+
+with open(textfile, 'r') as f:
     for line in f:
         awk = line.split(",")
         ip = awk[0]
@@ -21,9 +28,9 @@ with open('ls_ipport', 'r') as f:
             print('connected to ' + ip + ':' + port)
 
         except socket.error as msg:
-            print('failed to '+ ip + ':' + port + str(msg) +'\n')
+            print('failed to '+ ip + ':' + port + ' ' + str(msg) +'\n')
             pass
 
 s.close()
 f.close()
-print('\n### END OF TEST ###')
+print('### END OF TEST ###')
